624e10af9ead12854c06ddf6c9a22086748bb95e
Konstantin Strassheim <konstantin.strassheim@gmail.com>
2020-09-10 17:46:54 +0200

    init2

--- chest_p.py
+++ chest_p.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout, BatchNormalization
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import Callback, TensorBoard
@@ -18,7 +18,7 @@ except:
   # Invalid device or cannot modify virtual devices once initialized.
   pass
 
-PATH ='C:\\Users\\konst\\DS_Couse\\chest_xray'
+PATH ='C:\\projects\\chest_xray'
 
 batchSize = 32
 imgSize = 150
@@ -45,20 +45,24 @@ nn = Sequential()
 nn.add(Conv2D(32,3,padding='same', activation='relu',input_shape=(imgSize,imgSize,3)))
 nn.add(MaxPool2D())
 nn.add(Dropout(0.2))
+nn.add(BatchNormalization())
 nn.add(Conv2D(64, 3, padding='same',activation='relu'))
 nn.add(MaxPool2D())
 nn.add(Dropout(0.2))
+nn.add(BatchNormalization())
 nn.add(Conv2D(128, 3, padding='same',activation='relu'))
 nn.add(MaxPool2D())
 nn.add(Dropout(0.2))
+nn.add(BatchNormalization())
 nn.add(Conv2D(256, 3, padding='same',activation='relu'))
 nn.add(MaxPool2D())
 nn.add(Dropout(0.2))
+nn.add(BatchNormalization())
 nn.add(Flatten())
 nn.add(Dense(128,activation='relu'))
 nn.add(Dense(1,activation='sigmoid'))
 nn.summary()
-nn.compile(optimizer='rmsprop', loss=tf.keras.losses.BinaryCrossentropy(), metrics=['binary_accuracy'])
+nn.compile(optimizer='rmsprop', loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
 tensorboard = TensorBoard(log_dir='logs/{}'.format(time()))
 nn.fit(train_data_gen, validation_data=val_data_gen, epochs=3, batch_size=batchSize, validation_batch_size=16, callbacks=[tensorboard])
 
